646ae4be19767aee63c7ae9b441c3763bb3a9c42
rumahkodingku <rumahkodingku45@gmail.com>
2025-10-27 21:59:22 +0700

    first commit

---
+++ fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	firstName := "John"
+	lastName := "Doe"
+
+	fmt.Printf("Full Name: %s %s\n", firstName, lastName)
+}
