7da64ce206a4646340b4e99e54760fe0944578cd
rumahkodingku <rumahkodingku45@gmail.com>
2025-11-18 16:42:58 +0700

    learning package math

---
+++ math.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.40))
+	fmt.Println(math.Floor(1.99))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(math.Pow(2, 3))
+	fmt.Println(math.Max(10, 20))
+	fmt.Println(math.Min(10, 20))
+	fmt.Println(math.Abs(-42))
+}
